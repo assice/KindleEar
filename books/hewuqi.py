72ea771d7c75fd733ac7ff8b2cab0cbb65aa3d28
Assice <assicefc@gmail.com>
2020-04-05 16:49:58 +0800

    Add files via upload

--- books/hewuqi.py
+++ books/hewuqi.py
@@ -17,7 +17,7 @@ class hewuqi(BaseFeedBook):
     coverfile             = "cv_2048.jpg"# 设定封面图片
     oldest_article        = 1  # 设定文章的时间范围。小于等于365则单位为天，否则单位为秒，0为不限制
     max_articles_per_feed = 40 # 设定每个主题下要最多可抓取的文章数量
-    
+    fulltext_by_readability = True    
     
     # 指定要提取的包含文章列表的主题页面链接
     # 每个主题是包含主题名和主题页面链接的元组
